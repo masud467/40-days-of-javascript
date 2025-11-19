41cd1e518eaaf492f111c5ad5a5eba7574b54893
masud467 <masudpfrnt@gmail.com>
2025-11-20 02:34:50 +0600

    Updat Day-02

--- day-02/script.js
+++ day-02/script.js
@@ -1,2 +1,23 @@
 let person = "Babul";
-console.log(person);
+let age = 27;
+let isStudent = true;
+let favoriteProgrammingLanguage = "JS";
+console.log(person, age, isStudent, favoriteProgrammingLanguage);
+
+let address = "Dhaka";
+address = "Cumilla";
+console.log(address);
+
+const salary = 30000;
+salary = 40000;
+console.log(salary);
+
+// object
+const obj = {
+  name: "masud",
+  age: 28,
+  address: "Kalir Bazar",
+};
+
+// array
+const arr = [1, 2, 3, 4, 5];
